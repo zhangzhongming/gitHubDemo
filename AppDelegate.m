8fe8de97553668cd418acbc63917cac0d6c60c56
zhangzhongming <zhangzhongming@zhangzhongmingdeMacBook-Pro.local>
2016-03-18 09:44:56 +0800

    file changed
    app delegate changed

--- AppDelegate.m
+++ AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // this is file change test
+    
     return YES;
 }
 
